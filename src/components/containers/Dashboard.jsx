eb2f9cb673098150479c091d04eb32f67a1faf64
mike <mwilli26@gmail.com>
2020-05-25 17:25:25 -0400

    show twitch stream on fridays; update deploy scripts

--- src/components/containers/Dashboard.jsx
+++ src/components/containers/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Grid, Paper,
+  Grid, Paper, Typography, Tooltip,
 } from '@material-ui/core';
 import ReactPlayer from 'react-player';
 
@@ -28,12 +28,24 @@ class Dashboard extends Component {
 
   render() {
     const { classes } = this.props;
-    // const vidUrl = '../../videos/NetworkPromo.mp4';
+    // show twitch stream on fridays, I guess
+    // TODO: integrate with twitch API to get stream status
+    //       and show the twitch stream if live
+    const showTwitch = new Date().getDay() === 5;
+    const vidUrl = showTwitch ? 'https://www.twitch.tv/elffawm' : networkPromo;
+    const lightPlayer = !showTwitch;
+    const tooltipText = showTwitch ? '' : 'Click to get hyped';
+    const streamHeight = showTwitch ? 800 : '';
     return (
       <BaseApp>
         <Grid container spacing={2} justify="center">
+          <Typography variant="h3" style={{ fontVariant: 'small-caps' }}>
+            Welcome to the Rocket League League league site!
+          </Typography>
           <Paper className={classes.paper}>
-            <ReactPlayer url={networkPromo} light controls width="" height="" />
+            <Tooltip title={tooltipText}>
+              <ReactPlayer url={vidUrl} light={lightPlayer} controls width="" height={streamHeight} />
+            </Tooltip>
           </Paper>
         </Grid>
       </BaseApp>
