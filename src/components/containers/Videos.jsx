8d2e323791e6b3101364bc33d11daa2516af6dd6
mike <mwilli26@gmail.com>
2020-06-30 16:54:49 -0400

    update videos list

--- src/components/containers/Videos.jsx
+++ src/components/containers/Videos.jsx
@@ -9,15 +9,20 @@ import BaseApp from './BaseApp';
 import PageHeader from '../PageHeader';
 
 const allVideos = [
-  { name: 'Season 1 - Promo', googleUrl: 'https://drive.google.com/file/d/1xvSitSxdw-GHjqJtuGXQ82Lu72XGtDDy' },
-  { name: 'Season 1 - Playoffs', googleUrl: 'https://drive.google.com/file/d/141UnvusEkQ-CMK0V0bxsDpUpglcFqlkK' },
-  { name: 'Season 1 - HotCops Banner Raising Ceremony', googleUrl: 'https://drive.google.com/file/d/1Hsog1vtIVya1wlsgm3PrQCBJuwgycCmD' },
-  { name: 'Season 1 - Gameweek 2 report', googleUrl: 'https://drive.google.com/file/d/1LqxgOEdBdUiTjN1OfIaTebaohSV1aE0c' },
-  { name: 'Season 1 - Gameweek 3 report', googleUrl: 'https://drive.google.com/file/d/1OKS4u0HZgCbDt92SdnFEWwqm0k65dKjS' },
-  { name: 'Season 1 - Gameweek 4 report', googleUrl: 'https://drive.google.com/file/d/1qt4BD3MxBGrIB35XnZFZOE4fclVGI6G5' },
-  { name: 'Season 1 - Gameweek 5 report', googleUrl: 'https://drive.google.com/file/d/1QNdMQZD6xoaOgvMBh4LI8Qnc6HWqRq-v' },
-  { name: 'Season 2 - Draft (pre-trades)', googleUrl: 'https://drive.google.com/file/d/12w98pbZAp0BPZW_9atR6KL7BVxsJYUlh' },
+  { name: 'Season 2 - Playoffs', googleUrl: 'https://drive.google.com/file/d/1xhR_fDMQcV2xzIVF6WAjUilU5U0vEv14' },
+  { name: 'Season 2 - Gameweek 4 Report', googleUrl: 'https://drive.google.com/file/d/1qGekU7sYz2aUywj6p5DsS57lTACiQdKt' },
+  { name: 'Season 2 - Gameweek 3 Report', googleUrl: 'https://drive.google.com/file/d/1z8I9zWf9TJK_0m0EMlDPMtv1JiRzkV6F' },
+  { name: 'Season 2 - Gameweek 2 Report', googleUrl: 'https://drive.google.com/file/d/1pSHfXMHq_b9MBeYg_E7C84rcjKUIQGvH' },
+  { name: 'Season 2 - Gameweek 1 Report', googleUrl: 'https://drive.google.com/file/d/1vWogoOlKD-3iRKNq0eKAmhJp81F2K0hq' },
   { name: 'Season 2 - Promo', googleUrl: 'https://drive.google.com/file/d/1KeAw1knIU7Q8x-9GWHFVIDvlEiz82V3J' },
+  { name: 'Season 2 - Draft (pre-trades)', googleUrl: 'https://drive.google.com/file/d/12w98pbZAp0BPZW_9atR6KL7BVxsJYUlh' },
+  { name: 'Season 1 - HotCops Banner Raising Ceremony', googleUrl: 'https://drive.google.com/file/d/1Hsog1vtIVya1wlsgm3PrQCBJuwgycCmD' },
+  { name: 'Season 1 - Playoffs', googleUrl: 'https://drive.google.com/file/d/141UnvusEkQ-CMK0V0bxsDpUpglcFqlkK' },
+  { name: 'Season 1 - Gameweek 5 report', googleUrl: 'https://drive.google.com/file/d/1QNdMQZD6xoaOgvMBh4LI8Qnc6HWqRq-v' },
+  { name: 'Season 1 - Gameweek 4 report', googleUrl: 'https://drive.google.com/file/d/1qt4BD3MxBGrIB35XnZFZOE4fclVGI6G5' },
+  { name: 'Season 1 - Gameweek 3 report', googleUrl: 'https://drive.google.com/file/d/1OKS4u0HZgCbDt92SdnFEWwqm0k65dKjS' },
+  { name: 'Season 1 - Gameweek 2 report', googleUrl: 'https://drive.google.com/file/d/1LqxgOEdBdUiTjN1OfIaTebaohSV1aE0c' },
+  { name: 'Season 1 - Promo', googleUrl: 'https://drive.google.com/file/d/1xvSitSxdw-GHjqJtuGXQ82Lu72XGtDDy' },
 ];
 
 const useStyles = makeStyles((theme) => ({
